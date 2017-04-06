201ea6fb0947e680b00014f95441aacc28bf661b
Erik Vanderlei Fernandes <erik.vanderlei.programador@outlook.com>
2017-04-05 21:56:00 -0300

    Add and styling Logout component

---
+++ src/components/Auth/Logout.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="logout">
+    <div class="panel">
+      <h3 class="panel-title">Are you sure, you want to logout?</h3>
+      <p class="panel-description">The logout will disconnect you from the system!</p>
+      <div class="button-group">
+        <button class="button button-highlight">No</button>
+        <button class="button button-error">Yes</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {}
+</script>
+
+<style lang="scss" scoped>
+  .logout {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  .panel {
+    padding: 2em;
+    background: #ffffff;
+    border-radius: 3px;
+    box-shadow: 0 0 20px rgba(#243348, .3);
+  }
+  .button-group {
+    margin-bottom: 0;
+    display: flex;
+    justify-content: flex-end;
+  }
+  .button {
+    margin-right: 1em;
+    &:last-child {
+      margin-right: 0;
+    }
+  }
+  .panel-title {
+    color: #243348;
+  }
+  .panel-description {
+    color: #8287A1;
+    margin-top: .7em;
+  }
+</style>
--- src/routes/router.js
+++ src/routes/router.js
@@ -1,6 +1,7 @@
 import Home from '../components/Home/Home.vue';
 // Auth components
 import Login from '../components/Auth/Login.vue';
+import Logout from '../components/Auth/Logout.vue';
 import Register from '../components/Auth/Register.vue';
 // Dashboard Components
 import Dashboard from '../components/Dashboard/Main.vue';
@@ -13,6 +14,7 @@ const routes = [
   { path: '/login', name: 'login', component: Login },
   { path: '/register', name: 'register', component: Register },
   { path: '/dashboard', name: 'dashboard', component: Dashboard },
+  { path: '/logout', name: 'logout', component: Logout },
   { path: '*', component: PageNotFound }
 ];
 
