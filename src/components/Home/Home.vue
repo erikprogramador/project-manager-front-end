0c759aef8f0d80497252bb6ec0bab8fc6d4a01e1
Erik Vanderlei Fernandes <erik.vanderlei.programador@outlook.com>
2017-04-05 21:35:38 -0300

    Styling home component

--- src/components/Home/Home.vue
+++ src/components/Home/Home.vue
@@ -1,5 +1,16 @@
 <template>
-  <div>Home</div>
+  <main class="home">
+    <h1 class="title">PJM - Project Manager</h1>
+    <h2 class="subtitle">Organize yours clients</h2>
+    <div class="button-group">
+      <router-link to="/login">
+        <a class="button button-cta">Login</a>
+      </router-link>
+      <router-link to="/register">
+        <a class="button button-highlight button-cta">Register</a>
+      </router-link>
+    </div>
+  </main>
 </template>
 
 <script>
@@ -7,5 +18,22 @@
 </script>
 
 <style lang="scss" scoped>
-
+  .home {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+  }
+  .title, .subtitle {
+    color: #243348;
+  }
+  .title {
+    font-size: 2.5rem;
+  }
+  .subtitle {
+    letter-spacing: 5px;
+    margin: .2em 0 .5em;
+  }
 </style>
