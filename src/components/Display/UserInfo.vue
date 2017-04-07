01637eeab1e9ba3a4bc43e97583bdb12a44ea78e
Erik Vanderlei Fernandes <erik.vanderlei.programador@outlook.com>
2017-04-06 23:35:08 -0300

    Add information and styling userinfo component

--- src/components/Display/UserInfo.vue
+++ src/components/Display/UserInfo.vue
@@ -1,11 +1,45 @@
 <template>
-  <div>Herererer</div>
+  <ul>
+    <li class="list-row">
+      <h4 class="list-title">Username:</h4>
+      <span class="list-info">{{ user.username }}</span>
+    </li>
+    <li class="list-row">
+      <h4 class="list-title">Last Login:</h4>
+      <span class="list-info">{{ lastLogin }}</span>
+    </li>
+    </li>
+    <li class="list-row">
+      <h4 class="list-title">Permission:</h4>
+      <span class="list-info">{{ user.permission }}</span>
+    </li>
+  </ul>
 </template>
 
 <script>
-  export default {}
+  export default {
+    data () {
+      return {
+        user: {
+          username: 'admin',
+          permission: 'Adiministrator'
+        },
+        lastLogin: '3 days ago'
+      };
+    }
+  }
 </script>
 
 <style lang="scss" scoped>
-
+  .list-row {
+    padding: 1em;
+  }
+  .list-title {
+    margin-bottom: .5em;
+  }
+  .list-info {
+    padding-left: .5em;
+    font-size: 1.3rem;
+    color: lighten(#243348, 20%)
+  }
 </style>
