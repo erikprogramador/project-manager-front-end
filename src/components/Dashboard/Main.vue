9874f19f4de3d0129f925d33dbfa57a840256c18
Erik Vanderlei Fernandes <erik.vanderlei.programador@outlook.com>
2017-04-06 22:32:04 -0300

    Display info on the dashboard

--- src/components/Dashboard/Main.vue
+++ src/components/Dashboard/Main.vue
@@ -1,11 +1,44 @@
 <template>
-  <div>Heres is the dashboard</div>
+  <main>
+    <h1 class="page-header">Dashboard</h1>
+    <!-- That will be dynamic when i get the functional phase -->
+    <section class="card-container">
+      <info-card title="Projects" value="1">
+        <img src="../../assets/links/projector-screen.svg" alt="Projects" title="Projects">
+      </info-card>
+      <info-card title="Products" value="6">
+        <img src="../../assets/links/airplay.svg" alt="Products" title="Products">
+      </info-card>
+      <info-card title="Paid Products" value="$ 0.00">
+        <img src="../../assets/links/check-all.svg" alt="Paid Products" title="Paid Products">
+      </info-card>
+      <info-card title="Payment Pending" value="$ 0.00">
+        <img src="../../assets/links/alert-circle-outline.svg" alt="Payment Pending" title="Payment Pending">
+      </info-card>
+    </section>
+    <section>
+      <h2>Figure out</h2>
+      <ul>
+        <li>User graphs</li>
+        <li>Display pending payments</li>
+      </ul>
+    </section>
+  </main>
 </template>
 
 <script>
-  export default {}
+  import InfoCard from '../Display/InfoCard.vue';
+  export default {
+    components: { InfoCard }
+  }
 </script>
 
 <style lang="scss" scoped>
-
+  .card-container {
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    margin: 2em 0;
+  }
 </style>
---
+++ src/components/Display/InfoCard.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="info-card">
+    <slot></slot>
+    <div class="card-info">
+      <h3>{{ value }}</h3>
+      <span>{{ title }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ['title', 'value']
+  }
+</script>
+
+<style lang="scss" scoped>
+  .info-card {
+    width: 22%;
+    background: #ffffff;
+    padding: 1.2em 1em;
+    border-radius: 3px;
+    box-shadow: 0 5px 10px rgba(#243348, .2);
+    display: flex;
+    color: #243348;
+  }
+  .card-info {
+    margin-left: 1em;
+  }
+</style>
