e4b9a3062cabe08be6bea5b680a3c10f3adb05ff
Erik Vanderlei Fernandes <erik.vanderlei.programador@outlook.com>
2017-04-07 22:25:16 -0300

    Add components to Users, Projects and Customers

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,11 +1,22 @@
 import Home from '../components/Home/Home.vue';
+
 // Auth components
 import Login from '../components/Auth/Login.vue';
 import Logout from '../components/Auth/Logout.vue';
 import Register from '../components/Auth/Register.vue';
-// Dashboard Components
+
+// Dashboard components
 import Dashboard from '../components/Dashboard/Main.vue';
 import Settings from '../components/Dashboard/Settings.vue';
+
+// User components
+import User from '../components/User/Main.vue';
+
+// Project components
+import Project from '../components/Project/Main.vue';
+
+// Customer components
+import Customer from '../components/Customer/Main.vue';
 
 // Errors
 import PageNotFound from '../components/Error/PageNotFound.vue';
@@ -14,6 +25,9 @@ const routes = [
   { path: '/', name: 'home', component: Home },
   { path: '/login', name: 'login', component: Login },
   { path: '/register', name: 'register', component: Register },
+  { path: '/users', name: 'users', component: User },
+  { path: '/projects', name: 'projects', component: Project },
+  { path: '/customers', name: 'customers', component: Customer },
   { path: '/dashboard', name: 'dashboard', component: Dashboard },
   { path: '/settings', name: 'settings', component: Settings },
   { path: '/logout', name: 'logout', component: Logout },
