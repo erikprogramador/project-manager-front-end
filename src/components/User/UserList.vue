d38d558c8274291776dedbc7a5071a89cd169dfe
Erik Vanderlei Fernandes <erik.vanderlei.programador@outlook.com>
2017-04-10 22:12:58 -0300

    Styling the list user component

--- src/components/User/UserList.vue
+++ src/components/User/UserList.vue
@@ -1,26 +1,44 @@
 <template>
-  <table>
-    <thead>
-      <tr>
-        <th>#</th>
-        <th>Name</th>
-        <th>Type</th>
-        <th>Status</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td>1</td>
-        <td>Erik Vanderlei</td>
-        <td>erik@erik.com</td>
-        <td>Active</td>
-      </tr>
-    </tbody>
-  </table>
+  <div>
+    <input type="search" placeholder="Search" v-model="search">
+    <table>
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Name</th>
+          <th>Type</th>
+          <th>Status</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="user in users" :class="{ inactive: user.status === 'Inactive' }" @click="rowClicked">
+          <td>{{ user.id }}</td>
+          <td>{{ user.username }}</td>
+          <td>{{ user.type }}</td>
+          <td>{{ user.status }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
-  export default {}
+  export default {
+    data () {
+      return {
+        users: [
+          { id: 1, username: 'Erik Fernandes', type: 'Administrator', status: 'Active' },
+          { id: 2, username: 'Erik Vanderlei', type: 'Customer', status: 'Inactive' }
+        ],
+        search: ''
+      }
+    },
+    methods: {
+      rowClicked () {
+        console.log('Row is clicked')
+      }
+    }
+  }
 </script>
 
 <style lang="scss">
