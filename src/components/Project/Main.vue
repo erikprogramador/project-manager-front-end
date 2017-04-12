8acdab8854317ce09d630a429beff84b8af34337
Erik Vanderlei Fernandes <erik.vanderlei.programador@outlook.com>
2017-04-11 22:15:56 -0300

    Add Projects List component

--- src/components/Project/Main.vue
+++ src/components/Project/Main.vue
@@ -1,10 +1,13 @@
 <template>
   <main class="projects">
     <h1 class="page-header">Projects</h1>
+    <button class="button button-float button-top-right">
+      <img src="../../assets/links/light-plus.svg" alt="Add Project">
+    </button>
     <div class="panel-container">
       <panel :fullWidth="true">
         <h3 slot="header">All Projects</h3>
-        <table></table>
+        <project-list />
       </panel>
     </div>
   </main>
@@ -12,8 +15,9 @@
 
 <script>
   import Panel from '../Display/Panel.vue'
+  import ProjectList from './ProjectList.vue'
   export default {
-    components: { Panel }
+    components: { Panel, ProjectList }
   }
 </script>
 
---
+++ src/components/Project/ProjectList.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <input type="search" placeholder="Search" v-model="search">
+    <table>
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Name</th>
+          <th>Customer</th>
+          <th>Price</th>
+          <th>Status</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="project in projects" :class="{ inactive: project.status === 'Inactive' }" @click="rowClicked">
+          <td>{{ project.id }}</td>
+          <td>{{ project.name }}</td>
+          <td>{{ project.customer.name }}</td>
+          <td>{{ project.price }}</td>
+          <td>{{ project.status }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        projects: [
+          { id: 1, name: 'PJM', customer: { name: 'Erik Vanderlei' }, price: 2000.00, status: 'Active' },
+          { id: 2, name: 'SalesCity', customer: { name: 'Erik Fernandes' }, price: 3500.00, status: 'Inactive' }
+        ]
+      }
+    },
+    methods: {
+      rowClicked () {
+        console.log('Row clicked')
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+
+</style>
