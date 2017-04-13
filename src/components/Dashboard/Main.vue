4aca6ea9ef8666095c2897ac8f301b142c1721b6
Erik Vanderlei Fernandes <erik.vanderlei.programador@outlook.com>
2017-04-13 16:33:06 -0300

    Add components to the dashboard

--- src/components/Dashboard/Main.vue
+++ src/components/Dashboard/Main.vue
@@ -16,20 +16,53 @@
         <img src="../../assets/links/alert-circle-outline.svg" alt="Payment Pending" title="Payment Pending">
       </info-card>
     </section>
+    <div class="divisor"></div>
     <section>
-      <h2>Figure out</h2>
-      <ul>
-        <li>Put two rows with panels, where the first row will be (the currents projects, and pending payments projects), and the secod (customer with more financial return, customers with debit)</li>
-        <li>Display pending payments</li>
-      </ul>
-    </section>
-  </main>
+      <div class="panel-container">
+        <panel>
+          <h3 slot="header">Current projects</h3>
+          <simple-table :columns="['id', 'name', 'price']" :items="projects"/>
+        </panel>
+        <panel>
+          <h3 slot="header">Pending Projects</h3>
+          <simple-table :columns="['id', 'name', 'price']" :items="projects" :error="true"/>
+        </panel>
+      </div>
+    </div>
+    <div class="panel-container">
+      <panel>
+        <h3 slot="header">Customers</h3>
+        <simple-table :columns="['id', 'name', 'returnMoney']" :items="customers"/>
+      </panel>
+      <panel>
+        <h3 slot="header">Debtors Customers</h3>
+        <simple-table :columns="['id', 'name', 'returnMoney']" :items="customers" :error="true"/>
+      </panel>
+    </div>
+  </section>
+</main>
 </template>
 
 <script>
   import InfoCard from '../Display/InfoCard.vue'
+  import Panel from '../Display/Panel.vue'
+  import SimpleTable from '../Display/SimpleTable.vue'
   export default {
-    components: { InfoCard }
+    components: { InfoCard, Panel, SimpleTable },
+    data () {
+      return {
+        projects: [
+          { id: 1, name: 'PJM', price: 2000.00 },
+          { id: 2, name: 'ErikProgramador', price: 2500.00 },
+          { id: 3, name: 'SalesCity', price: 25000.00 }
+        ],
+        customers: [
+          { id: 1, name: 'Erik Vanderlei', returnMoney: 500000.00 },
+          { id: 2, name: 'Erik Fernandes', returnMoney: 508000.00 },
+          { id: 3, name: 'Vanderlei Fernandes', returnMoney: 480000.00 },
+        ]
+      }
+    }
   }
 </script>
 
---
+++ src/components/Display/SimpleTable.vue
@@ -0,0 +1,24 @@
+<template>
+  <table>
+    <thead>
+      <tr>
+        <th v-for="column in columns">{{ column }}</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="item in items" :class="{ inactive: inactive, error: error}">
+        <td v-for="column in columns">{{ item[column] }}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+  export default {
+    props: ['columns', 'items', 'inactive', 'error']
+  }
+</script>
+
+<style lang="scss">
+
+</style>
